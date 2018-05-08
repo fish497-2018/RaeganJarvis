0062102bff1b919034bab760da52a8e3cf6f87f8
Raegan Jarvis <raeganejarvis@gmail.com>
2018-05-08 13:56:23 -0700

    Filtered out fish with scale lengths of less than 1

--- Scripts/fish-analysis.R
+++ Scripts/fish-analysis.R
@@ -4,3 +4,7 @@ fish_data <- read.csv("Data/Gaeta_etal_CLC_data.csv")
 library(dplyr)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
+
+View(fish_data_cat)
+fish_mod <- filter(fish_data_cat, scalelength >= 1)
+View(fish_mod)
