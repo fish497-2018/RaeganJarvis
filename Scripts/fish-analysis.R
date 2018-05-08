de6ea1f496740edccf896044d967dca3ae7c5f60
Raegan Jarvis <raeganejarvis@gmail.com>
2018-05-08 13:44:53 -0700

    Described the purpose of the code

--- Scripts/fish-analysis.R
+++ Scripts/fish-analysis.R
@@ -1,5 +1,6 @@
 fish_data <- read.csv("Data/Gaeta_etal_CLC_data.csv")
 
+#this creates a new table categorizing fish by size
 library(dplyr)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
