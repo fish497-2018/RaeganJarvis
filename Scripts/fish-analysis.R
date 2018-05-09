4fe8d9d7c48f03ad5be843b1de4cd0d801341e24
Raegan Jarvis <raeganejarvis@gmail.com>
2018-05-08 17:04:55 -0700

    Included comments about which code corresponds to which exercise

--- Scripts/fish-analysis.R
+++ Scripts/fish-analysis.R
@@ -1,3 +1,5 @@
+# Exercise 2: First Solo Commit
+
 # Read in fish data
 fish_data <- read.csv("Data/Gaeta_etal_CLC_data_1.csv")
 
@@ -6,15 +8,21 @@ library(dplyr)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 
+# Exercise 3: Second Solo Commit
+
 # fliters out scalelengths of less than 1
 View(fish_data_cat)
 fish_mod <- filter(fish_data_cat, scalelength >= 1)
 View(fish_mod)
 
+# Exercise 5: Pushing Changes
+
 # creates scatterplot of fish length vs scalelength
 library(tidyverse)
 ggplot(fish_mod) +
   geom_point(aes(x = length, y = scalelength, color = lakeid))
+
+# Exercise 6: Pulling & Pushing
 
 #what about something like this
 # Great idea!
